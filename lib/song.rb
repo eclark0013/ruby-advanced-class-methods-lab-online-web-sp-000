28c24bfa64e57ca19966f64edbcdb642f781201c
Eric Clark <eclark0013@gmail.com>
2019-06-24 19:20:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,9 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
+  def self.new_by_name(the_name)
     song=self.new
-    song.name=name
+    song.name=the_name
   end
 
   def self.create_by_name(name)
